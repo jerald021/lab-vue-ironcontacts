8bd015909c40e0095135916bdbc6f6be609ce9e7
Jerald Osco <jerald021@gmail.com>
2022-03-17 21:19:17 +0100

    1r commit

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,32 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+<div id="app">
+    <h2>Iron Contacts</h2>
+
+    <td>
+      <th>Picture</th>
+      <th>Name</th>
+      <th>Popularity</th>
+    </td>    
+    <table class="table" v-for="contact in myContacts">
+
+  <tr>
+    <td><img class="picture" :src="contact.pictureUrl" alt=""></td>
+    <td>{{ contact.name }}</td>
+    🏆
+    <td>{{ contact.popularity.toFixed(2) }}</td>
+  </tr>
+    </table>
+  
+</div>
+  
 </template>
+
+<script setup>
+import contacts from "./contacts.json";
+
+const myContacts = contacts;
+myContacts.splice(5);
+</script>
 
 <style>
 #app {
@@ -17,5 +36,10 @@ import HelloWorld from './components/HelloWorld.vue'
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.table{
+border: 3px;}
+.picture{
+  width: 20%;
 }
 </style>
